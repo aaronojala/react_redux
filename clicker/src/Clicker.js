e3e696d5203be3d5858b95fbf0ed8f195fe9e2f9
Aaron Ojala <aaronojala@yahoo.com>
2021-01-26 17:33:47 +0200

    redux practice

--- clicker/src/Clicker.js
+++ clicker/src/Clicker.js
@@ -1,43 +1,37 @@
 import React, { Component } from 'react';
 import './Clicker.css'
+import {connect} from 'react-redux';
+import * as actionType from './actions/actions';
 
 class Clicker extends Component {
-  state = {
-    likes: 0,
-  };
-
-  addHandler = () => {
-    this.setState({
-      likes: this.state.likes + 1
-    });
-  };
-
-  removeHandler = () => {
-    this.setState({
-      likes: this.state.likes===0 ? 0 : this.state.likes -1
-    });
-  };
-
-  resetHandler = () => {
-    this.setState({
-      likes: 0
-    });
-  };
-
   render() {
     return (
       <div className="app">
         <div className="header">
-          <h1>Number of likes: {this.state.likes}</h1>
+          <h1>Number of likes: {this.props.ctr}</h1>
         </div>
         <div className="buttons">
-          <button onClick={this.addHandler}>Add like</button>
-          <button onClick={this.removeHandler}>Remove like</button>
-          <button onClick={this.resetHandler}>Reset counter</button>
+          <button onClick={this.props.onIncCounter}>Add like</button>
+          <button onClick={this.props.onDecCounter}>Remove like</button>
+          <button onClick={this.props.reset}>Reset counter</button>
         </div>
       </div>
     );
   }
 }
 
-export default Clicker;
+const mapStateToProps = (state) => {
+    return {
+        ctr: state.counter, // ctr = counter lyhennys
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncCounter: () => dispatch({type: actionType.INCREMENT}),
+        onDecCounter: () => dispatch({type: actionType.DECREASE}),
+        reset: () => dispatch({type: actionType.RESET})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Clicker);
--- clicker/src/index.js
+++ clicker/src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore} from 'redux';
+import reducer from './reducers/reducer';
+import {Provider} from 'react-redux';
+
+const store = createStore(reducer); // reducer(s) goes inside of store
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
